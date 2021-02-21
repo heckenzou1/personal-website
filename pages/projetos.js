4c23a6e5dd47d47ca66e05518cbcf481b5c6f93d
heitor <heitorzinhoclapton@gmail.com>
2021-02-21 11:11:40 -0300

    um

--- pages/projetos.js
+++ pages/projetos.js
@@ -5,17 +5,23 @@ import Headerzada from "./header";
 
 export default function Projetos() {
   return (
-    <div className={styles.container}>
+      <div className={styles.container}>
       <Headerzada />
       <div className="box">
+        <p style={{fontSize: '24px', paddingTop: '8px', fontFamily: 'Montserrat', fontWeight: 'bold'}}>This portfolio</p>
+
+        <div id="item" style={{backgroundImage: `url(${'/ummm.png'})`}}>
+        </div>
+        <p style={{fontSize: '17px'}}>Portfolio pessoal feito com nextjs<br />
+        será atualizado conforme necessidade<br /> e desenvolvimento de habilidades minhas</p>
         <div id="item"></div>
         <div id="item"></div>
         <div id="item"></div>
         <div id="item"></div>
-        <div id="item"></div>
-      </div>
+        </div>
+
       <Footer />
-    </div>
+      </div>
   );
 }
 
